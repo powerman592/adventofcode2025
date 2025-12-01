70e63bab54614181e46d23c0aef58593af9c4723
powerman592 <jayjayman592@gmail.com>
2025-12-01 01:48:55 -0500

    First part of day one challenge solved

---
+++ day1/process_input.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main()
+{
+    FILE * file_ptr;
+
+    // open file
+    file_ptr = fopen("puzzle_input.txt", "r");
+
+    if (file_ptr == NULL) {
+        printf("The file is not opened.");
+        return 1;
+    }
+
+    // The lock can goes from 0-99 and starts at position 50
+    // We need to process the combination and determine the number of times the dial is left pointing at 0.
+    int dial_position = 50u;
+    uint32_t answer = 0u;
+    uint16_t num_of_puzzle_inputs = 4186u;
+
+    while(!feof(file_ptr))
+    {
+        char * string;
+        char direction;
+        // Determine direction
+        direction = fgetc(file_ptr);
+        if ((direction != 'L') && (direction != 'R'))
+        {
+            printf("%c", direction);
+            return 12;
+        }
+
+        // Determine number of turns
+        fgets(string, 10, file_ptr);
+
+        // Remove newline character from string
+        // Unsure if this is necessary?
+        // string[sizeof(string) - 1u] = NULL;
+
+        int num_of_turns = atoi(string);
+        if ('L' == direction)
+        {
+            int a = dial_position - num_of_turns;
+            dial_position = (((a % 100) + 100) % 100);
+        }
+        else if ('R' == direction)
+        {
+            dial_position = ((dial_position + num_of_turns) % 100);
+        }
+        if (0 == dial_position)
+        {
+            answer++;
+        }
+    }
+    printf("%d", answer);
+
+
+    return 0;
+}
